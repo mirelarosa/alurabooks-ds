303768d7a9536ba44ab3970bed8e710822e3c9c5
Mirela Rosa <mirelar91@gmail.com>
2023-02-20 12:35:44 -0300

    adicionando github action

--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,8 @@ export const Thing: FC<Props> = ({ children }) => {
 };
 
 export * from './componentes/AbBotao'
+export * from './componentes/Card'
+export * from './componentes/AbTag'
+export * from './componentes/AbGrupoOpcoes'
+export * from './componentes/CampoTexto'
+
